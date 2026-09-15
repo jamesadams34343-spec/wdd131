996b44865dbb391cb27aebc3b1a2c271dd8d4cb4
jamesadams34343-spec <jamesadams34343@gmail.com>
2026-09-15 02:34:49 -0600

    Week one work first Program

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// const year = document.querySelector("current-year");
+// const yearElement = document.getElementById('current-year');
+document.getElementById("current-year").textContent = new Date().getFullYear();
+
+
+
+document.getElementById("lastModified").textContent = document.lastModified;
